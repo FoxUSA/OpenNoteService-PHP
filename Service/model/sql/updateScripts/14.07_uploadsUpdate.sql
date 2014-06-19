a0e74a1fc0f66040210f03f86cbe3f997b149283
FoxUSA <jake@jakelis.com>
2014-06-18 17:43:02 -0700

    Closes #42

--- Service/model/sql/updateScripts/14.07_uploadsUpdate.sql
+++ Service/model/sql/updateScripts/14.07_uploadsUpdate.sql
@@ -1,10 +1,44 @@
-USE test;
 
-START TRANSACTION;
+-- Change upload id to varchar
+	ALTER TABLE `uploads` CHANGE `id` `id` VARCHAR( 128 ) NOT NULL ;
 
-	-- Change notes to new id
-		UPDATE			note n-- yes I do mean to do a cartesian join
-			SET n.note = REPLACE (n.note,  CONCAT('?uploadID=',''), newID)
-		WHERE n.note LIKE '%?uploadID=%';
+-- Create token table
+	CREATE TABLE IF NOT EXISTS `token` (
+	  `id` int(11) NOT NULL AUTO_INCREMENT,
+	  `userID` int(11) NOT NULL,
+	  `ip` varchar(256) NOT NULL,
+	  `token` varchar(256) NOT NULL,
+	  `issued` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	  `expires` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+	  PRIMARY KEY (`id`),
+	  UNIQUE KEY `token` (`token`),
+	  KEY `userID` (`userID`)
+	) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=176 ;
 
-ROLLBACK;
+	ALTER TABLE `token`
+		ADD CONSTRAINT `token_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+		
+-- Remove old columns
+	ALTER TABLE `users` DROP `lastLoginIP`; -- This data can now be found in the token table
+	ALTER TABLE `users` DROP `lastLoginTime`;
+	
+	
+	
+/*
+
+-- Original Direct access
+UPDATE note
+	SET note = REPLACE(note,"http://domain/OpenNote/upload/", "https://domain/OpenNote/Service/upload/");
+
+UPDATE note
+	SET note = REPLACE(note,"https://domain/OpenNote/upload/", "https://domain/OpenNote/Service/upload/");
+
+-- Through upload script
+UPDATE note
+	SET note = REPLACE(note,"http://domain/OpenNote/upload/Download.php?uploadID=", "https://domain/OpenNote/Service/file/");
+
+UPDATE note
+	SET note = REPLACE(note,"https://domain/OpenNote/upload/Download.php?uploadID=", "https://domain/OpenNote/Service/file/");
+
+
+*/
